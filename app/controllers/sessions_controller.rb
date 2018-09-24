8ef634ef7f9b21d09d149c33284580611a45e83c
Daniel Hernandez <elreymcd@yahoo.com>
2018-09-24 09:59:42 -0500

    add sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+        @user.try(:authenticate, params[:password])
+        if @user
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            redirect_to new_session_path
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        redirect_to '/'
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :sessions
   resources :comment_updates
   resources :guests
   resources :stores
