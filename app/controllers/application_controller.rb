4cc21da322bc372bb5c1fb93bbc095ecdd28b09a
Daniel Hernandez <elreymcd@yahoo.com>
2018-09-24 15:53:35 -0500

    add bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     before_action :current_user
-    before_action :require_log_in, except: [:new, :create, :home]
+    #before_action :require_log_in, except: [:new, :create, :home]
     helper_method :current_user
+
+    def welcome
+    end
 
     def logged_in?
        !!current_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  root 'sessions#new'
-  resources :sessions
+  root 'sessions#welcome'
   resources :comments
   resources :comment_updates
   resources :guests
   resources :stores
   resources :users
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/signup' => 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
